b721db9fcf95d8ca5d932587fe785d0889d64c72
manikandan <manikandanvs1994@gmail.com>
2021-07-27 14:48:46 +0000

    added more lines on javaproj

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,26 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello World, DevOps has arrived!" to the terminal window.
         System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
+        System.out.println("Hello World, DevOps has arrived!");
     }
 
 }
